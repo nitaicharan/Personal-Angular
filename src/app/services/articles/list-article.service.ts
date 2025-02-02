395613571b6e804b2aa814f7f3a8b4f9621fa991
Nitai Charan <nitaicharan@gmail.com>
2025-02-02 16:16:24 +0000

    feat(articles): add `@Injectable` in `ListArticleService`

--- src/app/services/articles/list-article.service.ts
+++ src/app/services/articles/list-article.service.ts
@@ -1,5 +1,9 @@
+import { Injectable } from '@angular/core';
 import { ListBaseService } from '../base/list-base.service';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class ListArticleService extends ListBaseService {
   override get endpoint(): string {
     return '/articles';
