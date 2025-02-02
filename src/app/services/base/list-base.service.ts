4e80ca3bb4e1e8b2130dbd53aa2bc88d04ee9420
Nitai Charan <nitaicharan@gmail.com>
2025-02-02 16:17:08 +0000

    feat(base): remove rxjs `of` from base list service

--- src/app/services/base/list-base.service.ts
+++ src/app/services/base/list-base.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { Injectable } from '@angular/core';
-import { from, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,43 +12,5 @@ export abstract class ListBaseService {
 
   execute() {
     return this.httpClient.get<any[]>(environment.BASE_URL + this.endpoint);
-    // return of<any>({
-    //   articles: [
-    //     {
-    //       slug: 'how-to-train-your-dragon',
-    //       title: 'How to build webapps that scale',
-    //       description: 'This is a description post.',
-    //       tagList: ['realworld', 'implementations'],
-    //       createdAt: '2016-02-18T03:22:56.637Z',
-    //       updatedAt: '2016-02-18T03:48:35.824Z',
-    //       favorited: false,
-    //       favoritesCount: 1,
-    //       author: {
-    //         username: 'jake',
-    //         bio: 'I work at statefarm',
-    //         image: 'http://i.imgur.com/Qr71crq.jpg',
-    //         following: false,
-    //       },
-    //     },
-    //     {
-    //       slug: 'how-to-train-your-dragon-2',
-    //       title:
-    //         "The song you won't ever stop singing. No matter how hard you try",
-    //       description: 'This is another post description.',
-    //       tagList: ['dragons', 'training'],
-    //       createdAt: '2016-06-18T03:22:56.637Z',
-    //       updatedAt: '2016-06-18T03:48:35.824Z',
-    //       favorited: false,
-    //       favoritesCount: 23,
-    //       author: {
-    //         username: 'jake',
-    //         bio: 'I work at statefarm',
-    //         image: 'http://i.imgur.com/N4VcUeJ.jpg',
-    //         following: false,
-    //       },
-    //     },
-    //   ],
-    //   articlesCount: 2,
-    // });
   }
 }
